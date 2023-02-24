7bb8bfe7590302f81046b1e4714b5b36c1660d76
anommudita <gusmangananada@gmail.com>
2023-02-24 16:38:54 +0800

    upload rest_client_guzzle

--- pertemuan_9/README.md
+++ pertemuan_9/README.md
@@ -1,2 +1,9 @@
 # REST_API
 Belajar REST API bareng WEBUNPAS
+
+```python
+No Code 
+1   test1
+2   test2
+```
+
--- pertemuan_9/guzzle.php
+++ pertemuan_9/guzzle.php
@@ -8,11 +8,40 @@ use GuzzleHttp\Client;
 
 $client = new Client();
 
-
-
 $response = $client->request('GET', 'http://www.omdbapi.com/', [
+    // query untuk mengirimkan paramms yang ada di postman
     'query' => [
         'apikey' => 'd891a33a',
         's' => 'transformers'
     ]
 ]);
+
+
+$result = json_decode($response->getBody()->getContents(), true);
+// var_dump($result);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guzzle PHP</title>
+</head>
+
+<body>
+    <?php foreach ($result['Search'] as $movie) : ?>
+        <ul>
+            <li>Title : <?= $movie['Title'] ?></li>
+            <li>Year : <?= $movie['Year'] ?></li>
+            <li>
+                <img src="<?= $movie['Poster'] ?>" alt="" width="80">
+            </li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+
+</html>
